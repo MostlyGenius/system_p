4ecca1c9c137221a4e1258b39160092d8013c7f2
Server_Head <90004142+MostlyGenius@users.noreply.github.com>
2023-05-06 20:29:20 -0700

    update

--- audiomax99.go
+++ audiomax99.go
@@ -9,9 +9,22 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"syscall"
 	"time"
 )
+
+//remove unwanted values
+func removeUnwantedValues(s string) string {
+	unwanted := []string{"/", "(",")","~",">","<","@"} // list of unwanted values
+
+	// remove unwanted values from the string
+	for _, u := range unwanted {
+		s = strings.ReplaceAll(s, u, "")
+	}
+
+	return s
+}
 
 // delete unused file
 func removeFile(fileDelete string) {
@@ -83,9 +96,59 @@ func renameFile(number int) string {
 
 }
 
+// variable to store the last message sent to Discord
+var lastSent string
+
+
 // push to discord
 func pushDiscord(toSend string) {
+	// list of possible beginning values
+	beginnings := []string{"Time Remaining", "Begin B", "Begin C", "Begin D"}
+	// list of possible ending values
+	endings := []string{"(D) Mark this item for later review.", "Mark this item for later review", "Provide question feedback", "2023 KRYTERION"}
 
+	// remove the first part of the string based on the list of possible beginning values
+	for _, b := range beginnings {
+		if strings.HasPrefix(toSend, b) {
+			toSend = strings.TrimPrefix(toSend, b)
+			break
+		}
+	}
+
+	// remove the last part of the string based on the list of possible ending values
+	for _, e := range endings {
+		if strings.HasSuffix(toSend, e) {
+			toSend = strings.TrimSuffix(toSend, e)
+			break
+		}
+	}
+
+	// check if the current message is the same as the last message sent to Discord
+	if toSend == lastSent {
+		return
+	}
+
+	//remove unwanted values
+	toSendClean := removeUnwantedValues(toSend)
+
+	// send the processed string to Discord
+	discord.WebhookURL = "https://discord.com/api/webhooks/1234567890/abcdefg"
+	discord.Say(toSendClean)
+
+	// update the lastSent variable
+	lastSent = toSend
+}
+	}
+
+	// remove the last part of the string based on the list of possible ending values
+	for _, e := range endings {
+		if strings.HasSuffix(toSend, e) {
+			toSend = strings.TrimSuffix(toSend, e)
+			break
+		}
+	}
+
+	// send the processed string to Discord
 	discord.WebhookURL = "https://discord.com/api/webhooks/1090832857536143390/n1OgUi4MQ9u-Wz1sjAz0i3rRjKptDKoWZlNyx24U7T-9TQ04owLidBDoYOVPB7XL92mb"
 	discord.Say(toSend)
 }
